960f38989a4989d9f95f391458a9d2c2eb206504
hurera-swiftcontrol <hueraranjha9@gmail.com>
2026-04-22 10:52:24 -0400

    Added a logging file where each user input is tracked

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,48 @@
 package com.pluralsight;
 
-public class Main {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        String input = "";
+        try {
+            FileWriter fileWriter = new FileWriter("src/main/resources/log.txt");
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+
+            String todayFmt = updatedDateTime();
+            bufWriter.write(todayFmt + " launch \n");
+
+            while (!input.equals("X")) {
+                System.out.print("Enter a search term (X to exit): ");
+                input = myScanner.nextLine();
+                if (input.equals("X")) {
+                    break;
+                }
+                todayFmt = updatedDateTime();
+                bufWriter.write(todayFmt + " : " + input + "\n");
+            }
+            todayFmt = updatedDateTime();
+            bufWriter.write(todayFmt + " exit");
+            bufWriter.close();
+        }
+        catch(IOException e){
+            System.err.println("U did something wrong");
+
+        }
+
+    }
+
+    public static String updatedDateTime()
+    {
+        LocalDateTime today = LocalDateTime.now();
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return today.format(fmt);
+    }
 }
